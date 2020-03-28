ee0900fb9454fff71f5eb8437384fd06d3040105
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-03-28 18:07:14 +0530

    User Authentication Enabled

--- FirstProj/accounts/urls.py
+++ FirstProj/accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name = 'register')
+    path('register', views.register, name = 'register'),
+    path('login', views.login, name = 'login')
 ]
--- FirstProj/accounts/views.py
+++ FirstProj/accounts/views.py
@@ -12,7 +12,7 @@ def register(request):
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
-
+        cond1 = cond2 = False
         if User.objects.filter(username=username).exists():
                 messages.info(request, 'Username taken')
                 print('Username exists')
@@ -51,11 +51,10 @@ def login(request):
         user = auth.authenticate(username = username, password = password)
         if (user is not None):
             auth.login(request, user)
-            return redirect('/')
+            return redirect("/")
         else:
             messages.info(request, "Credentials incorrect")
             return redirect('login')
-
 
     else:
         return render(request, 'login.html')
